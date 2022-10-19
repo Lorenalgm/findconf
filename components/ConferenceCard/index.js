4e3ebccebf6d4579e6bacf25703e575aedeb3937
luma <lumagoesmontes@gmail.com>
2022-10-18 22:01:51 -0300

    add conference card date

--- components/ConferenceCard/index.js
+++ components/ConferenceCard/index.js
@@ -1,24 +1,26 @@
 
 import styles from './conferenceCard.module.css'
 import { FormatService } from "../../src/services/FormatService";
+import { format, parseISO } from 'date-fns';
 
-export default function ConferenceCard({conference}) {
+export default function ConferenceCard({ conference }) {
     return (
         <div className={styles.conferenceCard}>
-             <div className={styles.header}>
-                <img src='https://github.com/lumamontes.png' alt='conference logo'/>
+            <div className={styles.header}>
+                <img src='https://github.com/lumamontes.png' alt='conference logo' />
                 <h3>{conference.title}</h3>
-             </div>
+                <small>{format(parseISO(conference.inicial_date), 'dd/MM')}</small>
+            </div>
 
-             <div className={styles.label}>
-                 <span>{conference.type}</span>
-                 <span>{conference.price}</span>
-                 <span>{conference.language}</span>
-                 <span>{conference.field}</span>
-             </div>
-             {conference.description != '' && <p>{FormatService.limitText(conference.description, 70)}</p>}
+            <div className={styles.label}>
+                <span>{conference.type}</span>
+                <span>{conference.price}</span>
+                <span>{conference.language}</span>
+                <span>{conference.field}</span>
+            </div>
+            {conference.description != '' && <p>{FormatService.limitText(conference.description, 70)}</p>}
 
-             <button className={styles.button}>Visitar evento</button>
+            <button className={styles.button}>Visitar evento</button>
         </div>
     )
 }
