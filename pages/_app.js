60599fef6cbfca0704dca012eac7616ea4c5fd74
Lorena <lorena@proesc.com>
2023-03-16 17:28:10 -0300

    Adicionado script e layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import { NextIntlProvider } from "next-intl";
-import Navbar from "../components/Navbar";
 import "../styles/globals.css";
+import Script from 'next/script';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
  return (
@@ -9,9 +10,23 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title> Find Conf </title>
       </Head>
-
-     <Navbar />
-     <Component {...pageProps} />
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.GOOGLE_CODE}`}/>
+      <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-${process.env.GOOGLE_CODE}', {
+      page_path: window.location.pathname,
+      });
+      `,
+      }} />
+     <Layout>
+      <Component {...pageProps} />
+     </Layout>
    </NextIntlProvider>
  );
 }
